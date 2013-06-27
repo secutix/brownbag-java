b4013407c1cda55a5672963bf0b936799c958c06
Daniel Bany <dan.bany@gmail.com>
2013-06-27 12:38:04 +0200

    Solution

--- Selenium/brownbag-selenium/src/main/java/com/secutix/brownbag/selenium/DashBoard.java
+++ Selenium/brownbag-selenium/src/main/java/com/secutix/brownbag/selenium/DashBoard.java
@@ -2,10 +2,9 @@ package com.secutix.brownbag.selenium;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import com.google.common.collect.Lists;
 
 public class DashBoard {
 
@@ -19,13 +18,25 @@ public class DashBoard {
 	}
 
 	public void clickOnTab(final String tabName) {
+		for (WebElement tab : getTabs()) {
+			if (tab.getText().equals(tabName)) {
+				tab.click();
+				break;
+			}
+		}
 	}
 
 	public List<WebElement> getTabs() {
-		return Lists.newArrayList();
+		return driver.findElements(By.cssSelector("#myTab li"));
 	}
 
 	public boolean showsContinuousTestingResults() {
+		final List<WebElement> titleElements = driver.findElements(By.className("box_title"));
+		for (WebElement titleElement : titleElements) {
+			if (titleElement.getText().contains("Continuous testing")) {
+				return true;
+			}
+		}
 		return false;
 	}
 
